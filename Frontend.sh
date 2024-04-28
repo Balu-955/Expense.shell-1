bfecaefc3b5e595f1941523ccd145202ed732c00
Siva837453 <sivaallaka777@gmail.com>
2024-04-28 18:50:54 +0530

    data types

--- Frontend.sh
+++ Frontend.sh
@@ -1,36 +1,7 @@
 #!/bin/bash
 
-
-USERID=$(id -u)
-TIMESTAMP=$(date +%f-%H-%M-%S)
-SCRIPTNAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-
-VALIDATE(){
-
-if [ $1 -ne 0 ]
-then
-    echo -e "$2...$R FAILURE $N"
-    exit 1
-else    
-    echo -e "$2...$G SUCCESS $N"
-
-fi
-
-}
-
-if [ $USERID -ne 0 ]
-then
-    echo " please super user to run this command"
-    exit 1 #manually exit if error comes
-else
-    echo " you are super user"
-fi
+source ./common.sh 
+cur_root
 
 dnf install nginx -y &>>$LOGFILE
 VALIDATE $? "Install Nginx"
--- backend_service.sh
+++ backend_service.sh
@@ -1,40 +1,7 @@
 #!/bin/bash
 
-
-USERID=$(id -u)
-TIMESTAMP=$(date +%f-%H-%M-%S)
-SCRIPTNAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-echo "please enter DB password"
-read -s "Mysql_password"
-
-
-VALIDATE(){
-
-if [ $1 -ne 0 ]
-then
-    echo -e "$2...$R FAILURE $N"
-    exit 1
-else    
-    echo -e "$2...$G SUCCESS $N"
-
-fi
-
-}
-
-
-
-if [ $USERID -ne 0 ]
-then
-    echo " please super user to run this command"
-    exit 1 #manually exit if error comes
-else
-    echo " you are super user"
-fi
+source ./common.sh 
+cur_root
 
 
 dnf module disable nodejs -y &>>$LOGFILE
@@ -88,7 +55,7 @@ VALIDATE $? "enable backend"
 dnf install mysql -y &>>$LOGFILE
 VALIDATE $? "Mysql client service installed"
 
-mysql -h 172.31.18.43 -uroot -p${Mysql_password} < /app/schema/backend.sql
+mysql -h 172.31.29.19 -uroot -p${Mysql_password} < /app/schema/backend.sql
 VALIDATE $? "schema loading"
 
 systemctl restart backend 
